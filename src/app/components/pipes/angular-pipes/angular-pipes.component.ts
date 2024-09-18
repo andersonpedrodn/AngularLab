dd4031ee3dc3781803a944f7c841af85c9de5cab
andersonPedro <andersonpedrodn@gmail.com>
2024-09-18 18:37:54 -0300

    Pipes customizados

--- src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -1,12 +1,17 @@
-import { CommonModule, DatePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { AsyncPipe, CommonModule, CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, PercentPipe, registerLocaleData, UpperCasePipe } from '@angular/common';
+import { Component, LOCALE_ID, signal } from '@angular/core';
+
+import { Observable, delay, of } from 'rxjs';
+import { CustomStringPipe } from '../../../pipes/custom-string.pipe';
+
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [CommonModule, DatePipe],
+  imports: [CommonModule, DatePipe, UpperCasePipe, LowerCasePipe, JsonPipe, AsyncPipe, CurrencyPipe, DecimalPipe, PercentPipe, CustomStringPipe],
   templateUrl: './angular-pipes.component.html',
-  styleUrl: './angular-pipes.component.scss'
+  styleUrl: './angular-pipes.component.scss',
+  
 })
 export class AngularPipesComponent {
   someText = "TESTANDO PIPE OPERATOR";
