d7fee42d33692061a87eda1e88893f959a9c5642
andersonPedro <andersonpedrodn@gmail.com>
2024-09-27 11:57:14 -0300

    Estudo sobre template driven forms, ciração de form submit e select

--- src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss'
 })
 export class TemplateDrivenFormsComponent {
+  public listComidas = signal<Array<{ comida: string; preco: string }>>([
+    {
+      comida: 'X-salada',
+      preco: 'R$ 10',
+  },
+  {
+    comida: 'X-bacon',
+    preco: 'R$ 11'
+  },
+  {
+    comida: 'Coxinha',
+    preco: 'R$ 6',
+  },   
+]);
 
+public submitForm(form: NgForm) {
+  console.log(form.valid);
+  if(form.valid){
+    console.log(form.valid);
+  }
+}
 }
